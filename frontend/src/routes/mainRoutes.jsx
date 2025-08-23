61ade1fd6f53551efc304a2949d41bec0471b207
eushaahmed08 <eushaahmed08@gmail.com>
2025-08-23 22:44:17 +0600

    Created StudentDashboard

--- frontend/src/routes/mainRoutes.jsx
+++ frontend/src/routes/mainRoutes.jsx
@@ -2,10 +2,15 @@ import { Route } from "react-router-dom";
 import Homepage from "../pages/home/homepage";
 import LoginPage from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
-import AboutUs from "../components/About/AboutUs";
-import PrivacyPolicy from "../pages/Privaacy/Privaacy.jsx";
-import Contact from "../components/Contact/Contact.jsx";
 import Profile from "../pages/Profile/Profile.jsx";
+import StudentDashboard from "../pages/StudentDashboard/StudentDashboard.jsx";
+import PrivacyPolicy from "../pages/Privaacy/Privaacy.jsx";
+import AboutUs from "../components/About/AboutUs";
+import Contact from "../components/Contact/Contact.jsx";
+
+
+
+
 const mainRoutes = [
   <Route key="home" path="/" element={<Homepage />} />,
   <Route key="login" path="/login" element={<LoginPage />} />,
@@ -14,6 +19,9 @@ const mainRoutes = [
   <Route key="privacy" path="/privacy" element={<PrivacyPolicy />} />,
   <Route key="contact" path="/contact" element={<Contact />} />,
   <Route key="profile" path="/profile" element={<Profile />} />,
+  <Route key="studentdashboard" path="/studentdashboard" element={<StudentDashboard/>} />,
+
+
 ];
 
 export default mainRoutes;
