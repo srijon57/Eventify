ad5e4d91f1650c6b700e5c2c7043cf2996433326
SQL Srijon <116680350+srijon57@users.noreply.github.com>
2025-08-24 20:54:03 +0600

    fixed

--- frontend/src/pages/EventPage/EventPage.jsx
+++ frontend/src/pages/EventPage/EventPage.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import api from "../../lib/api"; // Import the configured Axios instance
-
+import api from "../../lib/api"; 
 export default function EventPage() {
     const { id } = useParams();
     const navigate = useNavigate();
